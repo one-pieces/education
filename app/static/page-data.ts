d082da6b4022c980a7c771c5efc7346e7338a293
Xiaolong Lin <Xiaolong.Lin@blackboard.com>
2015-10-19 22:49:25 +0800

    add page-data.ts

---
+++ app/static/page-data.ts
@@ -0,0 +1,25 @@
+/// <reference path='../app.d.ts' />
+
+export = [
+    {
+        title: 'Goal',
+        content: 
+            'In this lesson we\'re going to have you master the following target langauge:<br/> +
+            '&nbsp;&nbsp;&nbsp;zài … … li / shɑnɡ<br/>' +
+            '&nbsp;&nbsp;&nbsp;在  … … 里 / 上<br/>' +
+            '&nbsp;&nbsp;&nbsp;wèi shén me ？ yīn wèi … …<br/>' +
+            '&nbsp;&nbsp;&nbsp;为  什   么 ？ 因  为  … …<br/>' +
+            '&nbsp;&nbsp;&nbsp;S+ yào +vp.<br/>' +
+            '&nbsp;&nbsp;&nbsp;S+ 要  +vp.<br/>' +
+            '<br/>' +
+            'By the end of the lesson you will have:<br/>' +
+            '1)&nbsp;&nbsp;&nbsp;&nbsp;Guessed the meaning from the context.<br/>' +
+            '2)&nbsp;&nbsp;&nbsp;&nbsp;Correctly pronounced the langauge.<br/>' +
+            '3)&nbsp;&nbsp;&nbsp;&nbsp;Used the language in real life situations.<br/>'
+    }, {
+        title: 'View the video, pay attention to the dialogue and learn through the context.',
+        videoURL: 'http://player.youku.com/player.php/sid/XMTM2MDQ5MzUxMg==/v.swf'
+    }, {
+        title: 'Select "restroom"'
+    }
+];
