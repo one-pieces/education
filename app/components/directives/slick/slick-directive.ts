d2a30c91e88283f58cee22777876ebf46ab532a5
Xiaolong Lin <Xiaolong.Lin@blackboard.com>
2015-11-08 20:59:09 +0800

    make slick-directive more multifunctional

--- app/components/directives/slick/slick-directive.ts
+++ app/components/directives/slick/slick-directive.ts
@@ -60,8 +60,8 @@ export var directiveName = 'opSlick';
  */
 
 export class SlickDirective implements ng.IDirective {
-    static $inject = ['$injector'];
-    constructor(private $injector: ng.auto.IInjectorService) { }
+    static $inject = ['$injector', '$timeout'];
+    constructor(private $injector: ng.auto.IInjectorService, $timeout: ng.ITimeoutService) { }
 
     restrict = 'AE';
     scope = {
@@ -110,9 +110,99 @@ export class SlickDirective implements ng.IDirective {
 
     link = (scope: any, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
         // var isInitialized = false;
+        var isInit = false;
         var slider: any = $(element);
 
+        scope.$on('$destroy', () => {
+            if (isInit) {
+                // Slick takes a list of elements and applies styles to turn it into a carousel. Calling unslick too early might cause flashes of
+                // unstyled content to appear to the user. This "unslick" method will typically be called when closing a panel. The longest
+                // "panel closing" animation in at the top of _offcanvas-panels.scss is 500ms long, so I'm adding a 500ms delay here to make sure
+                // to only unslick after the panel has been closed and prevent those flashes of unstyled content. See screenshots at LRN-101644 to
+                // get an idea of what this looks like to the user without this fix.
+                scope.$timeout(() => {
+                    slider.slick('unslick');
+                }, scope, 500);
+            }
+        });
+
+        var prevArrow = $(scope.prevArrow);
+        var nextArrow = $(scope.nextArrow);
+
+        function updateArrowState() {
+          if (prevArrow.hasClass('slick-disabled')) {
+            prevArrow.attr('disabled', 'disabled');
+          } else {
+            prevArrow.removeAttr('disabled');
+          }
+
+          if (nextArrow.hasClass('slick-disabled')) {
+            nextArrow.attr('disabled', 'disabled');
+          } else {
+            nextArrow.removeAttr('disabled');
+          }
+        }
+
         function initializeSlick() {
+            // setup the event listeners (which replace the callback methods starting with slick.js 1.4)
+            element.on('beforeChange', function(event: JQueryEventObject, sl: any, oldIndex: number, newIndex: number) {
+                if (scope.onBeforeChange) {
+                  scope.onBeforeChange({$event: event, $sl: sl, $oldIndex: oldIndex, $newIndex: newIndex});
+                }
+                //remove aria-hidden attribute to make screen reader work
+                element.find('.slick-slide').removeAttr('aria-hidden');
+            });
+
+            element.on('afterChange', function (event: JQueryEventObject, sl: any, newIndex: number) {
+                updateArrowState();
+
+                if (scope.onAfterChange) {
+                  scope.onAfterChange({$event: event, $sl: sl, $newIndex: newIndex});
+                }
+
+                element.find('.slick-slide').removeAttr('aria-hidden');
+
+                if (scope.$$phase || scope.$root.$$phase) {
+                  scope.currentIndex = newIndex;
+                } else {
+                  return scope.$apply(function () {
+                    return scope.currentIndex = newIndex;
+                  });
+                }
+            });
+
+            element.on('init', function (event: JQueryEventObject, sl: any) {
+                if (sl.slideCount <= sl.options.slidesToShow) {
+                  prevArrow.addClass('slick-disabled');
+                  nextArrow.addClass('slick-disabled');
+                }
+
+                if (scope.onInit) {
+                  scope.onInit();
+                }
+
+                element.find('.slick-slide').removeAttr('aria-hidden');
+
+                updateArrowState();
+
+                // Sometimes, when the page loads, a scrollbar is added, then removed once the panel is fully loaded, which results in wrong widths,
+                // so we need to manually fire a resize event here.
+                $(window).trigger('resize');
+
+                isInit = true;
+
+                if (scope.toUpdateSlide) {
+                    scope.toUpdateSlide = false;
+                    return sl.slideHandler(scope.currentIndex, false, true);
+                }
+            });
+
+            if (scope.onReInit) {
+                element.on('reInit', function (event: JQueryEventObject, sl: any) {
+                    scope.onReInit();
+                });
+            }
+
             slider.slick({
                 accessibility: scope.accessibility !== 'false',
                 arrows: scope.arrows !== 'false',
